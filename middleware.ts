875a4d387d32a7a44f4da644d77407abb9065e7b
0xflacko <dinokou01@gmail.com>
2025-09-04 19:08:13 +0100

    fix: Refine CSP in middleware.ts and remove nonce generation

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,22 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
-
   const csp = `
     default-src 'self';
-    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://connect.facebook.net https://www.googletagmanager.com;
+    script-src 'self' 'unsafe-eval' https://connect.facebook.net https://www.googletagmanager.com;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
     img-src 'self' data: https://www.facebook.com https://www.google-analytics.com;
     font-src 'self' https://fonts.gstatic.com;
-    connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://www.facebook.com https://graph.facebook.com;
+    connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://www.facebook.com https://graph.facebook.com https://region1.google-analytics.com;
     frame-src https://www.googletagmanager.com https://www.facebook.com;
   `;
 
   const response = NextResponse.next();
-  response.headers.set('Content-Security-Policy', csp.replace(/\n/g, ' ').trim());
+  response.headers.set(
+    "Content-Security-Policy",
+    csp.replace(/\n/g, " ").trim()
+  );
   return response;
 }
 
@@ -28,6 +29,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)?',
+    "/((?!api|_next/static|_next/image|favicon.ico).*)?",
   ],
 };
